b0bca2c98ce47a50c93d90e38f0d03d455e54a5b
Webmin <unconfigured@null.spigotmc.org>
2020-10-14 23:12:55 -0400

    Initial Commit

---
+++ Financial_Management_Project/GeneralTab.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Financial_Management_Project {
+    public partial class GeneralTab : Form {
+        public GeneralTab() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Financial_Management_Project/MortgageForm.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Forms;
+
+namespace Financial_Management_Project {
+    public partial class MortgageForm : Form {
+        public MortgageForm() {
+            InitializeComponent();
+        }
+    }
+}
